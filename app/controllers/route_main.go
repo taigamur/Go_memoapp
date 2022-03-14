e178eff22ca14698430b935814cee85c7e89c93e
Taiga Miura <miura.t84@gmail.com>
2022-03-14 19:46:30 +0900

    view template

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,11 +1,21 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
 func top(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	generateHTML(w, "Hello", "layout", "top")
 }
